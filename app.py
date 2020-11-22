062d8fbe41d32749217dab916fb46631298d043d
csaybar <csaybar@gmail.com>
2020-11-22 15:04:20 +0100

    buttoms added

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from src.map_display import * 
+from fileinput import FileInput
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-ee.Initialize()
+
+from src import ee_map
+from src import utils
+
+# Init Earth Engine
+utils.ee_Initialize()
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -16,3 +22,15 @@ async def home(request: Request):
 @app.get("/methodology")
 async def methodology(request: Request):
     return templates.TemplateResponse("methodology.html", {"request": request, "year":2020})
+
+@app.get("/map")
+async def map(request: Request):
+    ee_map.position(-77.08767, -11.97586, 18) 
+    ee_map.restart()
+    return templates.TemplateResponse("map_template.html", {"request": request})
+
+@app.post('/map')
+def form_post(request: Request):
+    ee_map.position(0,0,2)
+    ee_map.addTile(geoviz=None)
+    return templates.TemplateResponse('map_template.html', {'request': request})
---
+++ src/utils.py
@@ -0,0 +1,37 @@
+import os
+import ee
+
+# Copy the token to the container
+def ee_Initialize():
+    ee_token = os.environ['EARTHENGINE_TOKEN']
+    credential = '{"refresh_token":"%s"}' % ee_token
+    credential_file_path = os.path.expanduser("~/.config/earthengine/")
+    os.makedirs(credential_file_path,exist_ok=True)
+    with open(credential_file_path + 'credentials', 'w') as file:
+        file.write(credential)
+    try:
+        ee.Initialize()        
+    except Exception as e:
+        print(e)
+        return False    
+    return True
+
+
+def replace_line(file_name: str, line_num: int, text: str) -> bool:
+    """ Replace a specific line in a file
+
+    Args:
+        file_name (str): file path
+        line_num (int): line to replace.
+        text (str): Text to put in the line.
+
+    Returns:
+        bool: If the line was replaced will return True.
+    """
+    with open(file_name, 'r+') as f:
+        lines = f.readlines()
+        lines[line_num] = text
+        f.writelines(lines)
+    with open(file_name, 'w') as fw:    
+        fw.writelines(lines)
+    return True
