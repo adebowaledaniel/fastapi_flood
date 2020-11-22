f719b25ca30b4884524223c9bfd5a0de3a0a4b3f
csaybar <csaybar@gmail.com>
2020-11-22 10:29:52 +0100

    some changes more

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from src.map_display import * 
+from ee_token import set_heroku_vars
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+
 ee.Initialize()
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -16,3 +19,17 @@ async def home(request: Request):
 @app.get("/methodology")
 async def methodology(request: Request):
     return templates.TemplateResponse("methodology.html", {"request": request, "year":2020})
+
+@app.get("/map")
+async def map(request: Request):
+    basemap_addtext = "        //earthengine_space;\n" 
+    replace_line('templates/ee_map.html', 24, basemap_addtext)
+    return templates.TemplateResponse("map_template.html", {"request": request})
+
+@app.post('/map')
+def form_post(request: Request):
+    basemap = simple_eemap()
+    basemap_addtext = "        L.tileLayer('%s').addTo(map);\n" % basemap
+    replace_line('templates/ee_map.html', 24, basemap_addtext)
+    return templates.TemplateResponse('map_template.html', {'request': request})
+
